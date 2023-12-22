a39b7aa7c7f5bf9cfb8a98222352aeeb5af40ed0
hyun128 <pluto5338@gmail.com>
2023-12-23 00:44:18 +0100

    2signup

--- backend-nodejs/server.js
+++ backend-nodejs/server.js
@@ -116,7 +116,7 @@ app.get('/reservations', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
